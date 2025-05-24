6296135059b4c8df881705b2f097f17a57e721b5
Atharva Deo <deoatharva44@gmail.com>
2025-05-24 20:44:19 +0530

    frontend

--- save_small.py
+++ save_small.py
@@ -1,5 +1,3 @@
-from audiocraft.models import MusicGen
-<<<<<<< HEAD
 import torch
 
 # Load the pretrained MusicGen model
@@ -7,10 +5,3 @@ musicgen = MusicGen.get_pretrained('facebook/musicgen-small')
 
 # Save the underlying model weights
 torch.save(musicgen.lm.state_dict(), "save_small.pt")
-=======
-import torch 
-
-model = MusicGen.get_pretrained('facebook/musicgen-small')
-
-torch.save(model.lm.state_dict(),'small_model.pt')
->>>>>>> 00807cd (new)
